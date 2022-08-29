44bd93626610d6692b099c3240dc2e5c17ebb313
rachelschipull <rlschipull@gmail.com>
2022-08-29 12:28:22 -0400

    update with google auth

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 //Load config
 dotenv.config( { path: './config/config.env'})
+
+//Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -24,12 +29,24 @@ app.engine('.hbs', exphbs.engine({
     ));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(session({
+    secret:'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+//Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 //Static folder
 app.use(express.static('public'))
 
 //Routes
 app.use('/', require('./routes/index'))
-app.use('/dashboard', require('./routes/index'))
+//app.use('/dashboard', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 const PORT = process.env.PORT || 3000
 
